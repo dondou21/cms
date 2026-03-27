1cc3accafc08a6814a03f788d3e9212cf5b0addb
dondou21 <dondouabiyi@gmail.com>
2026-03-27 16:07:43 +0200

    chore: remove debug logging from authController

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -59,7 +59,6 @@ exports.login = async (req, res) => {
             }
         });
     } catch (error) {
-        console.error('Login error:', error);
         res.status(500).json({ message: 'Error logging in', error: error.message });
     }
 };
